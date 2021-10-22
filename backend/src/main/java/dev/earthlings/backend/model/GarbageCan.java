fdfc2d1a7728dd94989879626d58dfd606a59c0f
Paul Kim <kimpj16@farmingdale.edu>
2021-10-22 18:30:01 -0400

    Update GarbageCan.java
    Changed Latitude and Longitude to doubles

--- backend/src/main/java/dev/earthlings/backend/model/GarbageCan.java
+++ backend/src/main/java/dev/earthlings/backend/model/GarbageCan.java
@@ -5,8 +5,8 @@ import lombok.Data;
 @Data
 public class GarbageCan {
 
-    private String latitude;
-    private String longitude;
+    private double latitude;
+    private double longitude;
     private String createdBy;
     private String lastUpdated;
     private String createdTime;
